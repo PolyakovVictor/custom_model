b3c4f4f99bc5a2c57163ca98130ac5b741191022
PolyakovVictor <polyakov.victor21@gmail.com>
2025-07-17 22:17:21 +0300

    feat: added ReLU

---
+++ CustomNN.py
@@ -0,0 +1,5 @@
+class CustomNN:
+    def __init__(self,):
+        pass
+
+    def ReLU(self, number: int) -> int: return number if number > 0 else 0
---
+++ main.py
@@ -0,0 +1,6 @@
+from CustomNN import CustomNN
+
+if __name__ == '__main__':
+    nn = CustomNN()
+    result = nn.ReLU(5)
+    print(result)
